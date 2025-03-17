bd5737d50d86d816e12b7d028ca085254e9975e5
lijing <flm894634186@qq.com>
2025-03-17 13:40:34 +0800

    feat：小说章节、小说内容拉取相关功能实现，新增 websocket 接口实时向前端发送拉取进度

--- best-msc-cobweb/src/main/java/com/jing/msc/cobweb/CobwebApp8135.java
+++ best-msc-cobweb/src/main/java/com/jing/msc/cobweb/CobwebApp8135.java
@@ -3,6 +3,7 @@ package com.jing.msc.cobweb;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author : jing
